5040869f3dfb36d1c8da0d27c79a907fb16f03cc
deepmehra0 <deepakmehra0102@gmail.com>
2022-08-20 13:29:22 +0530

    added change

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_jwt import JWT
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///student_database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI','sqlite:///student_database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='DKSRDKS'
 api =Api(app)
