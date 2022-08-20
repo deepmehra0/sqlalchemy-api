135bf5b19e827a2c18d3483a8fada70d1b58f8fe
deepmehra0 <deepakmehra0102@gmail.com>
2022-08-20 13:07:19 +0530

    added change

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from flask import Flask
 from flask_restful import Api
 from securtity import authenticate,identity
 from flask_jwt import JWT
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///student_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///student_database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='DKSRDKS'
 api =Api(app)
